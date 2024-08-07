08acf9d645906cd79737525e0e5a5230c5573308
Marcos Pacanhela <marcosalexandrepacanhela71@gmail.com>
2024-08-07 20:42:29 -0300

    useState

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount((prevState) => prevState + 1)
+  }
+
+  const decrement = () => {
+    setCount((prevState) => prevState - 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.big}>{count}</Text>
+      <View style={styles.inline}>
+        <Button title='REMOVER' onPress={decrement}></Button>
+        <Button title='ADICIONAR' onPress={increment}></Button>
+      </View> 
     </View>
   );
 }
@@ -13,8 +25,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4939BA',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  enter:{
+    borderColor: 'white',
+    borderWidth: 1,
+    backgroundColor: '#5450D6',
+    width:'80%',
+    color:'white'  
+  },
+  big:{
+    fontSize:40
+  },
+  inline:{
+    width: "50%",
+    flexDirection:'row',
+    justifyContent: 'center'
+  }
 });
